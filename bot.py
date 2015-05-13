0dd4600d6ab66cecac2eaaa90f42a2a2d65020e4
Lukas Lozovski <lukas.lozovski@gmail.com>
2015-05-12 22:02:56 -0400

    update supervisor code

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ import chess_state
 import evaluators
 import minimax
 
+import time
+
 argparser = argparse.ArgumentParser()
 argparser.add_argument('--player', "-p", default="white", type=str, help="'[w]hite' or '[b]lack'; the bot player's color")
 
@@ -17,7 +19,19 @@ class Bot(object):
         self.state = chess_state.ChessState(evaluate=evaluate)
         self.player = player
         self.searchdepth = searchdepth
+        self.wins = 0
+        self.loses = 0
+        self.stalemates = 0
 
+    def reset_game(self):
+        chess_state.ChessState().reset()
+        self.state = chess_state.ChessState(evaluators.MaterialEvaluator())
+        self.player = chess.WHITE
+        self.searchdepth = 5
+        self.wins = 0
+        self.loses = 0
+        self.stalemates = 0
+        
     def choose_move(self):
         """Given the current board state, choose a best move."""
         _, move = minimax.minimax(self.state,
@@ -32,7 +46,54 @@ class Bot(object):
         """Modify the board state by making the given move."""
         self.state.push(move)
 
-def main(player=chess.WHITE, searchdepth=2):
+
+class Supervisor():
+    bots = []
+    def __init__(self, number):
+#        self.bots = []
+        for i in range(0, number):
+            self.bots.append(Bot())
+
+    def begin(self):
+        print("starting bot moves")
+
+        while True:
+            for bot in self.bots:
+                print(bot)
+                for other_bot in self.bots:
+                    print(other_bot)
+                    m1 = bot.choose_move()
+                    bot.make_move(m1)
+                    m2 = other_bot.choose_move()
+                    other_bot.make_move(m2)
+                    print(other_bot.state)
+                    if bot.state.is_game_over() or other_bot.state.is_game_over():
+                        print("game over!")
+                        bot.reset_game()
+                        other_bot.reset_game()
+                        
+                        break
+
+#        while True:
+#            m1 = self.bot1.choose_move()
+#            self.bot1.make_move(m1)
+#            m2 = self.bot2.choose_move()
+#            self.bot2.make_move(m2)
+
+#            print(self.bot2.state)
+
+#            if self.bot1.state.is_game_over() or self.bot2.state.is_game_over():
+#               print("game over!")
+#                break
+            
+            #time.sleep(2)
+
+def main():
+    supervisor = Supervisor(3)
+    supervisor.begin()
+
+    
+def main1(player=chess.WHITE, searchdepth=2):
     b = Bot(player=player, searchdepth=searchdepth)
     if player == chess.WHITE:
         # if the bot is white, make a first move.
@@ -53,7 +114,7 @@ def main(player=chess.WHITE, searchdepth=2):
         while not b.state.is_legal(m):
             print("Illegal move! Try again.\n")
             m = chess.Move.from_uci(input())
-
+ 
         # make the move
         b.make_move(m)
         print(b.state)
